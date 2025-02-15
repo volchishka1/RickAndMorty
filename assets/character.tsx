7c2aa39f6f033643d9c5b1056cae8c3fd0592ab0
macwolf <romazverb18@gmail.com>
2025-02-15 11:13:35 +0300

    added icons to bottom tabs

---
+++ assets/character.tsx
@@ -0,0 +1,13 @@
+import * as React from 'react';
+
+import Svg, { Path } from 'react-native-svg';
+export const CharacterSvg = () => (
+  <Svg viewBox={'0 -7 24 38'}>
+    <Path
+      fill={'#1441b2'}
+      d={
+        'M21.71 20.29 18 16.61A9 9 0 1 0 16.61 18l3.68 3.68a1 1 0 0 0 1.42 0 1 1 0 0 0 0-1.39ZM11 18a7 7 0 1 1 7-7 7 7 0 0 1-7 7Z'
+      }
+    />
+  </Svg>
+);
