d33bdd94a852ac89de62d1fde8032544c5e52b34
macwolf <romazverb18@gmail.com>
2025-02-15 11:14:17 +0300

    added config to metro

--- metro.config.js
+++ metro.config.js
@@ -1,4 +1,4 @@
-const {getDefaultConfig, mergeConfig} = require('@react-native/metro-config');
+const { getDefaultConfig, mergeConfig } = require('@react-native/metro-config');
 
 /**
  * Metro configuration
@@ -6,6 +6,10 @@ const {getDefaultConfig, mergeConfig} = require('@react-native/metro-config');
  *
  * @type {import('@react-native/metro-config').MetroConfig}
  */
+
+// const { wrapWithReanimatedMetroConfig } = require('react-native-reanimated/metro-config');
+
 const config = {};
 
+// module.exports = wrapWithReanimatedMetroConfig(config);
 module.exports = mergeConfig(getDefaultConfig(__dirname), config);
