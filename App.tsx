7233841b87f28b1f68b443a2dace7e1657c83efd
macwolf <romazverb18@gmail.com>
2025-02-15 11:57:59 +0300

    added provider to app.tsx

--- App.tsx
+++ App.tsx
@@ -3,11 +3,15 @@ import 'react-native-gesture-handler';
 import React from 'react';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
 import { Navigator } from '@navigation/navigator.tsx';
+import { Provider } from 'react-redux';
+import { store } from '@root/store/store.ts';
 
 export const App = () => {
   return (
-    <SafeAreaProvider>
-      <Navigator />
-    </SafeAreaProvider>
+    <Provider store={store}>
+      <SafeAreaProvider>
+        <Navigator />
+      </SafeAreaProvider>
+    </Provider>
   );
 };
