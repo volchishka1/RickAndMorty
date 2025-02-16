01f7bbff8f95ff127cb9ad249da4b913754a4592
macwolf <romazverb18@gmail.com>
2025-02-16 17:39:38 +0300

    deleted unnecessary route

--- src/navigation/types.ts
+++ src/navigation/types.ts
@@ -1,7 +1,6 @@
 import { ROUTES } from '@constants/routes';
 
 export type MainStackScreenNavigatorParamList = {
-  [ROUTES.MAIN_SCREEN]: undefined;
   [ROUTES.CHARACTERS_SCREEN]: undefined;
   [ROUTES.DETAILS_SCREEN]: undefined;
   [ROUTES.SETTING_SCREEN]: undefined;
