da7a3b99fdd29796f1252bbb0cb29df5497c1e6d
macwolf <romazverb18@gmail.com>
2025-02-16 03:18:24 +0300

    added asyncStorage function

---
+++ src/constants/asyncStorage.tsx
@@ -0,0 +1,28 @@
+import AsyncStorage from '@react-native-async-storage/async-storage';
+
+export const saveString = async (key, value) => {
+  try {
+    await AsyncStorage.setItem(key, value);
+    return true;
+  } catch (error) {
+    return false;
+  }
+};
+
+export const saveItem = async (key, value) => {
+  saveString(key, JSON.stringify(value));
+  console.log(`saveAsync${value}`);
+};
+
+export const getItem = async (key) => {
+  try {
+    const itemString = await AsyncStorage.getItem(key);
+    if (itemString) {
+      return JSON.parse(itemString);
+    } else {
+      return null;
+    }
+  } catch (error) {
+    return null;
+  }
+};
