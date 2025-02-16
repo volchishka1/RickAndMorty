5abd0e3dd402b190e6944d50061e1d55cd971ace
macwolf <romazverb18@gmail.com>
2025-02-16 19:00:07 +0300

    added layout to charactersScreen

--- src/screens/charactersScreen/charactersScreenView.tsx
+++ src/screens/charactersScreen/charactersScreenView.tsx
@@ -1,12 +1,19 @@
-import React from 'react';
-import { Text, View } from 'react-native';
+import React, { FC } from 'react';
+import { Text, TouchableOpacity, View } from 'react-native';
+import { CharactersScreenViewProps } from '@screens/charactersScreen/types.ts';
+import { charactersScreenViewStyle } from '@screens/charactersScreen/style.ts';
 
-export const CharactersScreenView = () => {
+export const CharactersScreenView: FC<CharactersScreenViewProps> = (props) => {
+  const { navigateToDetailsScreen } = props;
+
   return (
-    <View
-      style={{ top: '50%', justifyContent: 'center', alignItems: 'center', backgroundColor: 'red' }}
-    >
-      <Text>{'CharactersScreen'}</Text>
+    <View style={charactersScreenViewStyle.rootContainer}>
+      <TouchableOpacity
+        style={charactersScreenViewStyle.buttonStyle}
+        onPress={navigateToDetailsScreen}
+      >
+        <Text style={charactersScreenViewStyle.textButtonStyle}>{'Characters'}</Text>
+      </TouchableOpacity>
     </View>
   );
 };
