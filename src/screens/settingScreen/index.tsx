80dc852a2be1ddae8cfc780342625782b5d25ace
macwolf <romazverb18@gmail.com>
2025-02-16 18:05:03 +0300

    changed logic place from view to index

--- src/screens/settingScreen/index.tsx
+++ src/screens/settingScreen/index.tsx
@@ -1,6 +1,17 @@
 import React from 'react';
 import { SettingScreenView } from '@screens/settingScreen/settingScreenView.tsx';
+import { Appearance, useColorScheme } from 'react-native';
+import { saveItem } from '@constants/asyncStorage.tsx';
+import setColorScheme = Appearance.setColorScheme;
 
 export const SettingScreen = () => {
-  return <SettingScreenView />;
+  const scheme = useColorScheme();
+
+  // function for switch and add colorScheme of the app to asyncStorage
+  const switchTheme = () => {
+    scheme === 'dark' ? setColorScheme('light') : setColorScheme('dark');
+    scheme === 'dark' ? saveItem('colorScheme', 'light') : saveItem('colorScheme', 'dark');
+  };
+
+  return <SettingScreenView switchTheme={switchTheme} />;
 };
--- src/screens/settingScreen/settingScreenView.tsx
+++ src/screens/settingScreen/settingScreenView.tsx
@@ -1,23 +1,16 @@
-import React from 'react';
-import { Appearance, Button, Text, useColorScheme, View } from 'react-native';
-import setColorScheme = Appearance.setColorScheme;
-import { saveItem } from '@constants/asyncStorage.tsx';
+import React, { FC } from 'react';
+import { Text, TouchableOpacity, View } from 'react-native';
+import { SettingScreenViewProps } from '@screens/settingScreen/types.ts';
+import { settingScreenViewStyle } from '@screens/settingScreen/style.ts';
 
-export const SettingScreenView = () => {
-  const scheme = useColorScheme();
-
-  // function for switch and add colorScheme of the app to asyncStorage
-  const switchTheme = () => {
-    scheme === 'dark' ? setColorScheme('light') : setColorScheme('dark');
-    scheme === 'dark' ? saveItem('colorScheme', 'light') : saveItem('colorScheme', 'dark');
-  };
+export const SettingScreenView: FC<SettingScreenViewProps> = (props) => {
+  const { switchTheme = () => {} } = props;
 
   return (
-    <View
-      style={{ top: '50%', justifyContent: 'center', alignItems: 'center', backgroundColor: 'red' }}
-    >
-      <Text>{'SettingScreen'}</Text>
-      <Button title={'changeScheme'} onPress={switchTheme}></Button>
+    <View style={settingScreenViewStyle.rootContainer}>
+      <TouchableOpacity style={settingScreenViewStyle.buttonStyle} onPress={switchTheme}>
+        <Text style={settingScreenViewStyle.textButtonStyle}>{'Change color scheme'}</Text>
+      </TouchableOpacity>
     </View>
   );
 };
