99d8e17fbf7e47a738ed1aba1c55ab3f671c80ed
macwolf <romazverb18@gmail.com>
2025-02-16 03:22:39 +0300

    added function for switch colorTheme

--- src/screens/settingScreen/settingScreenView.tsx
+++ src/screens/settingScreen/settingScreenView.tsx
@@ -1,12 +1,23 @@
 import React from 'react';
-import { Text, View } from 'react-native';
+import { Appearance, Button, Text, useColorScheme, View } from 'react-native';
+import setColorScheme = Appearance.setColorScheme;
+import { saveItem } from '@constants/asyncStorage.tsx';
 
 export const SettingScreenView = () => {
+  const scheme = useColorScheme();
+
+  // function for switch and add colorScheme of the app to asyncStorage
+  const switchTheme = () => {
+    scheme === 'dark' ? setColorScheme('light') : setColorScheme('dark');
+    scheme === 'dark' ? saveItem('colorSchemeDark', scheme) : saveItem('colorSchemeLight', scheme);
+  };
+
   return (
     <View
       style={{ top: '50%', justifyContent: 'center', alignItems: 'center', backgroundColor: 'red' }}
     >
       <Text>{'SettingScreen'}</Text>
+      <Button title={'changeScheme'} onPress={switchTheme}></Button>
     </View>
   );
 };
