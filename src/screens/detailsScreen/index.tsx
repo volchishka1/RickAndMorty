8bd1d7f905c91967da1f3fe5fe5c0020d3b3d7bb
macwolf <romazverb18@gmail.com>
2025-02-16 18:59:05 +0300

    added nav to characterScreen in detailsScreen

--- src/screens/detailsScreen/index.tsx
+++ src/screens/detailsScreen/index.tsx
@@ -1,6 +1,19 @@
-import React from 'react';
+import React, { FC } from 'react';
 import { DetailsScreenView } from '@screens/detailsScreen/detailsScreenView.tsx';
+import { ROUTES } from '@constants/routes.ts';
+import { CompositeScreenProps } from '@react-navigation/native';
+import { MainStackScreenNavigatorParamList } from '@navigation/types.ts';
+import { StackScreenProps } from '@react-navigation/stack';
 
-export const DetailsScreen = () => {
-  return <DetailsScreenView />;
+export type DetailsScreenProps = CompositeScreenProps<
+  StackScreenProps<MainStackScreenNavigatorParamList, ROUTES.DETAILS_SCREEN>,
+  any
+>;
+
+export const DetailsScreen: FC<DetailsScreenProps> = ({ navigation }) => {
+  const backToCharacterScreen = () => {
+    navigation.navigate(ROUTES.CHARACTERS_SCREEN);
+  };
+
+  return <DetailsScreenView backToCharacterScreen={backToCharacterScreen} />;
 };
