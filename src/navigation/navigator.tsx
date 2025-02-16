8fbf743e973c976aebc6e5f9ffdd7a6be756237e
macwolf <romazverb18@gmail.com>
2025-02-16 03:18:48 +0300

    added theme config to navContainer

--- src/navigation/navigator.tsx
+++ src/navigation/navigator.tsx
@@ -1,9 +1,12 @@
-import { NavigationContainer } from '@react-navigation/native';
+import { DarkTheme, DefaultTheme, NavigationContainer } from '@react-navigation/native';
 import { MainScreenTabNavigator } from '@root/bottom-tabs/MainScreen.tsx';
+import { useColorScheme } from 'react-native';
 
 export const Navigator = () => {
+  const scheme = useColorScheme();
+
   return (
-    <NavigationContainer>
+    <NavigationContainer theme={scheme === 'dark' ? DefaultTheme : DarkTheme}>
       <MainScreenTabNavigator />
     </NavigationContainer>
   );
