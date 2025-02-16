abed53e18b1c764c3166a3f16856395c70792390
macwolf <romazverb18@gmail.com>
2025-02-16 03:19:26 +0300

    added callback getAppTheme to app Function

--- App.tsx
+++ App.tsx
@@ -1,12 +1,27 @@
 import 'react-native-gesture-handler';
 
-import React from 'react';
+import React, { useCallback, useEffect } from 'react';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
 import { Navigator } from '@navigation/navigator.tsx';
 import { Provider } from 'react-redux';
 import { store } from '@root/store/store.ts';
+import { getItem } from '@constants/asyncStorage.tsx';
+import { Appearance, useColorScheme } from 'react-native';
+import setColorScheme = Appearance.setColorScheme;
 
 export const App = () => {
+  // Callback for taking colorTheme from AsyncStorage
+  const scheme = useColorScheme();
+  const getAppTheme = useCallback(async () => {
+    const schemesDark = await getItem('colorSchemeDark');
+    const schemesLight = await getItem('colorSchemeLight');
+    scheme === 'light' ? setColorScheme(schemesLight) : setColorScheme(schemesDark);
+  }, []);
+
+  useEffect(() => {
+    getAppTheme();
+  }, [getAppTheme]);
+
   return (
     <Provider store={store}>
       <SafeAreaProvider>
