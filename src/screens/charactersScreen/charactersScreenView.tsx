abb2488ad770eb931dcd627b4573cff02c3d9d72
macwolf <romazverb18@gmail.com>
2025-02-18 23:29:36 +0300

    added charactersCardComponent to charactersScreen

--- src/screens/charactersScreen/charactersScreenView.tsx
+++ src/screens/charactersScreen/charactersScreenView.tsx
@@ -1,19 +1,31 @@
 import React, { FC } from 'react';
-import { Text, TouchableOpacity, View } from 'react-native';
+import { ScrollView, View } from 'react-native';
 import { CharactersScreenViewProps } from '@screens/charactersScreen/types.ts';
 import { charactersScreenViewStyle } from '@screens/charactersScreen/style.ts';
+import { ResponseData } from '@screens/detailsScreen';
+import { CharacterCardComponent } from '@root/component/characterCardComponent/characterCardComponent.tsx';
 
 export const CharactersScreenView: FC<CharactersScreenViewProps> = (props) => {
-  const { navigateToDetailsScreen } = props;
+  const { navigateToDetailsScreen = () => {}, results } = props;
 
   return (
     <View style={charactersScreenViewStyle.rootContainer}>
-      <TouchableOpacity
-        style={charactersScreenViewStyle.buttonStyle}
-        onPress={navigateToDetailsScreen}
-      >
-        <Text style={charactersScreenViewStyle.textButtonStyle}>{'Characters'}</Text>
-      </TouchableOpacity>
+      <ScrollView style={charactersScreenViewStyle.centerContainer}>
+        {results.map((item: ResponseData) => (
+          <CharacterCardComponent
+            onPress={() => navigateToDetailsScreen(item.id)}
+            itemId={item.id}
+            itemImage={item.image}
+            itemGender={item.gender}
+            itemOrigin={item.origin.name}
+            itemEpisode={item.episode[0]}
+            itemName={item.name}
+            itemStatus={item.status}
+            itemSpecies={item.species}
+            itemLocationName={item.location.name}
+          />
+        ))}
+      </ScrollView>
     </View>
   );
 };
