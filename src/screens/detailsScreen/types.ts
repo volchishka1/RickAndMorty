ee47df4aa31f17826be19ec990735af57a7b279d
macwolf <romazverb18@gmail.com>
2025-02-18 23:36:34 +0300

    added props types results, getCharId, getIsChar to detailsScreen

--- src/screens/detailsScreen/types.ts
+++ src/screens/detailsScreen/types.ts
@@ -2,4 +2,7 @@ import { TouchableOpacityProps } from 'react-native';
 
 export type DetailsScreenViewProps = {
   backToCharacterScreen: TouchableOpacityProps['onPress'];
+  results: any;
+  getCharacterId: number;
+  getIsCharacter: boolean;
 };
