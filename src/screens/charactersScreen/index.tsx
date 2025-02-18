961dd97ad39e8a3fe8b37b460f211e22cafbaf1d
macwolf <romazverb18@gmail.com>
2025-02-18 23:31:17 +0300

    added call allCharacters, setCharId, setChar, useState

--- src/screens/charactersScreen/index.tsx
+++ src/screens/charactersScreen/index.tsx
@@ -1,9 +1,12 @@
-import React, { FC } from 'react';
+import React, { FC, useEffect, useState } from 'react';
 import { CharactersScreenView } from '@screens/charactersScreen/charactersScreenView.tsx';
 import { CompositeScreenProps } from '@react-navigation/native';
 import { StackScreenProps } from '@react-navigation/stack';
 import { MainStackScreenNavigatorParamList } from '@navigation/types.ts';
 import { ROUTES } from '@constants/routes.ts';
+import { useDispatch, useSelector } from 'react-redux';
+import { setCharacterId, setIsCharacter } from '@root/store/slices.ts';
+import { loadAllCharacter } from '@root/store/api-actions.ts';
 
 export type CharactersScreenProps = CompositeScreenProps<
   StackScreenProps<MainStackScreenNavigatorParamList, ROUTES.CHARACTERS_SCREEN>,
@@ -11,9 +14,27 @@ export type CharactersScreenProps = CompositeScreenProps<
 >;
 
 export const CharactersScreen: FC<CharactersScreenProps> = ({ navigation }) => {
-  const navigateToDetailsScreen = () => {
+  const dispatch = useDispatch();
+
+  const [results, setResults] = useState([]);
+
+  const data = useSelector((state) => state.toolkit.characters);
+
+  const navigateToDetailsScreen = (index: number) => {
     navigation.navigate(ROUTES.DETAILS_SCREEN);
+    dispatch(setCharacterId(index - 1));
+    dispatch(setIsCharacter(true));
   };
 
-  return <CharactersScreenView navigateToDetailsScreen={navigateToDetailsScreen} />;
+  useEffect(() => {
+    dispatch(loadAllCharacter());
+  }, []);
+
+  useEffect(() => {
+    setResults(data);
+  }, [data]);
+
+  return (
+    <CharactersScreenView navigateToDetailsScreen={navigateToDetailsScreen} results={results} />
+  );
 };
