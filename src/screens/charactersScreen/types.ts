f871b2f08dde5aed2d4dd529a6dcda9eeb489bc7
macwolf <romazverb18@gmail.com>
2025-02-18 23:35:01 +0300

    added props results to charactersScreen

--- src/screens/charactersScreen/types.ts
+++ src/screens/charactersScreen/types.ts
@@ -1,5 +1,7 @@
 import { TouchableOpacityProps } from 'react-native';
+import { ResponseData } from '@screens/detailsScreen';
 
 export type CharactersScreenViewProps = {
   navigateToDetailsScreen: TouchableOpacityProps['onPress'];
+  results: Array<ResponseData>;
 };
