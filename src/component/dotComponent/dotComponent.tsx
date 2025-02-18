61592bc2d610668c0a2523d2f921c486c0009ddf
macwolf <romazverb18@gmail.com>
2025-02-18 23:29:58 +0300

    added dotComponent

---
+++ src/component/dotComponent/dotComponent.tsx
@@ -0,0 +1,22 @@
+import React, { FC } from 'react';
+import { View, ViewProps } from 'react-native';
+
+export type DotProps = {
+  status?: ViewProps['children'];
+};
+
+export const DotComponent: FC<DotProps> = (props) => {
+  const { status } = props;
+
+  return (
+    <View
+      style={{
+        backgroundColor: status === 'Alive' ? 'green' : 'red',
+        width: 10,
+        height: 10,
+        borderRadius: 10,
+        marginRight: 5,
+      }}
+    ></View>
+  );
+};
