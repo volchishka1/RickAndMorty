f7a018915af5767b89681b018863740ff5b2e0f3
macwolf <romazverb18@gmail.com>
2025-02-18 23:28:58 +0300

    added backSvg

---
+++ assets/back.tsx
@@ -0,0 +1,11 @@
+import * as React from 'react';
+
+import Svg, { Path } from 'react-native-svg';
+export const BackIconSvg = () => (
+  <Svg viewBox='0 0 25 30' width='35' height='35'>
+    <Path
+      fill={'#1441b2'}
+      d='M20 25a1 1 0 0 1-.71-.29l-8-8a1 1 0 0 1 0-1.42l8-8a1 1 0 1 1 1.42 1.42L13.41 16l7.3 7.29a1 1 0 0 1 0 1.42A1 1 0 0 1 20 25Z'
+    />
+  </Svg>
+);
