51138a267d976fec3517a4f8f39803b3f8f88025
macwolf <romazverb18@gmail.com>
2025-02-19 03:29:25 +0300

    added new type props to characterScreen

--- src/screens/charactersScreen/types.ts
+++ src/screens/charactersScreen/types.ts
@@ -3,5 +3,7 @@ import { ResponseData } from '@screens/detailsScreen';
 
 export type CharactersScreenViewProps = {
   navigateToDetailsScreen: TouchableOpacityProps['onPress'];
-  results: Array<ResponseData>;
+  onEndReached: any;
+  characters: Array<ResponseData>;
+  getIsLoading: boolean;
 };
