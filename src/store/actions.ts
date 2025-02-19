47ac323852030e99a84651fb322de1f5d395ecb0
macwolf <romazverb18@gmail.com>
2025-02-20 01:06:26 +0300

    deleted unnecessary files

--- src/store/actions.ts
+++
@@ -1,9 +0,0 @@
-import { createAction } from '@reduxjs/toolkit';
-import { ActionType } from '@root/type/action.ts';
-
-export const setIsLoadingData = createAction(
-  ActionType.SetIsLoadingData,
-  (setIsLoadingData: boolean) => ({
-    payload: setIsLoadingData,
-  }),
-);
--- src/store/homeScreen/selectors.ts
+++
@@ -1,4 +0,0 @@
-import { NameSpace, RootState } from '../root-reducer';
-
-export const getIsLoadingData = (state: RootState): boolean =>
-  state[NameSpace.homeScreen].setLoadingData;
