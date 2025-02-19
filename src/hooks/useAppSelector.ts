c4868ceb3cdfd12d6d23ea24965d2bef5f2a9db3
macwolf <romazverb18@gmail.com>
2025-02-20 02:17:14 +0300

    deleted unnecessary const

--- src/hooks/useAppSelector.ts
+++ src/hooks/useAppSelector.ts
@@ -1,8 +1,7 @@
-import { TypedUseSelectorHook, useDispatch, useSelector } from 'react-redux';
+import { TypedUseSelectorHook, useSelector } from 'react-redux';
 
 import { store } from '@root/store/store.ts';
 import { RootState } from '@root/store/root-reducer.ts';
 export type AppDispatch = typeof store.dispatch;
 
-export const useAppDispatch: () => AppDispatch = useDispatch;
 export const useAppSelector: TypedUseSelectorHook<RootState> = useSelector;
