8fa9340ca362308329e9c76551ea236aa516dad9
macwolf <romazverb18@gmail.com>
2025-02-19 03:27:04 +0300

    refactored api-action for saving pages

--- src/store/api-actions.ts
+++ src/store/api-actions.ts
@@ -1,15 +1,16 @@
 import { ThunkActionResult } from '@root/type/action.ts';
-import { saveCharacters } from '@root/store/slices.ts';
+import { saveCharacters, setIsLoading } from '@root/store/slices.ts';
 
 export const loadAllCharacter =
-  (): ThunkActionResult =>
+  (page: number): ThunkActionResult =>
   async (dispatch, _getState, api): Promise<void> => {
-    const apiUrl = `https://rickandmortyapi.com/api/character/`;
+    const apiUrl = `https://rickandmortyapi.com/api/character?page=${page}`;
     api
       .get(apiUrl)
       .then((response) => {
         const resCharacters = response.data.results;
         dispatch(saveCharacters(resCharacters));
+        dispatch(setIsLoading(false));
       })
       .catch((error) => error);
   };
