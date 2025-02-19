1dd17f1b71cbc3c29969c1b7098e38ec6e36f2f1
macwolf <romazverb18@gmail.com>
2025-02-20 01:12:05 +0300

    added dropdownComponent

---
+++ src/component/dropDownComponent/dropdownComponent.tsx
@@ -0,0 +1,39 @@
+import React from 'react';
+import { Dropdown } from 'react-native-element-dropdown';
+import { BackIconSvg } from '@assets/back.tsx';
+import { setValueItem } from '@root/store/slices.ts';
+import { useDispatch } from 'react-redux';
+import { dropdownStyles } from '@root/component/dropDownComponent/styles.ts';
+
+const data = [
+  { label: 'Cancel', value: '' },
+  { label: 'Alive', value: 'Alive' },
+  { label: 'Dead', value: 'Dead' },
+  { label: 'unknown', value: 'unknown' },
+  { label: 'Alien', value: 'Alien' },
+  { label: 'Human', value: 'Human' },
+];
+
+export const DropdownComponent = () => {
+  const dispatch = useDispatch();
+
+  return (
+    <Dropdown
+      style={dropdownStyles.dropdown}
+      placeholderStyle={dropdownStyles.placeholderStyle}
+      selectedTextStyle={dropdownStyles.selectedTextStyle}
+      inputSearchStyle={dropdownStyles.inputSearchStyle}
+      iconStyle={dropdownStyles.iconStyle}
+      data={data}
+      search
+      maxHeight={250}
+      labelField='label'
+      valueField='value'
+      placeholder='Select item'
+      onChange={(item) => {
+        dispatch(setValueItem(item.value));
+      }}
+      renderLeftIcon={() => <BackIconSvg />}
+    />
+  );
+};
