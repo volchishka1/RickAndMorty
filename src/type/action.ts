f5e83eaf49bf737fd036891682249bf3129d0be8
macwolf <romazverb18@gmail.com>
2025-02-20 02:16:31 +0300

    deleted unnecessary type from action

--- src/type/action.ts
+++ src/type/action.ts
@@ -4,10 +4,6 @@ import { ThunkAction } from 'redux-thunk';
 
 import { RootState } from '../store/root-reducer';
 
-export enum ActionType {
-  SetIsLoadingData = 'user/SetIsLoadingData',
-}
-
 export type ThunkActionResult<R = Promise<void | boolean>> = ThunkAction<
   R,
   RootState,
