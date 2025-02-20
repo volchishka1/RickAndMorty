7eba3be0444c676add5bb34d16b2b040736e68b1
macwolf <romazverb18@gmail.com>
2025-02-20 11:55:36 +0300

    deleted unnecessary prop

--- src/screens/detailsScreen/types.ts
+++ src/screens/detailsScreen/types.ts
@@ -3,6 +3,5 @@ import { TouchableOpacityProps } from 'react-native';
 export type DetailsScreenViewProps = {
   backToCharacterScreen: TouchableOpacityProps['onPress'];
   character: any;
-  getCharacterId: number;
   getIsCharacter: boolean;
 };
